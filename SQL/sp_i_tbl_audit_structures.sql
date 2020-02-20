e3606dbc669825e332c74769efe6e80198a7469d
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-02-20 08:04:29 -0500

    Updated the audit structures stored procedure to include the notes column. This column is important when reviewing structures in tbl_check_structures. Closes #4.

--- SQL/sp_i_tbl_audit_structures.sql
+++ SQL/sp_i_tbl_audit_structures.sql
@@ -3,7 +3,7 @@
  Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
  Modified By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management   																						   			          
  Created Date:  01/13/2020																							   
- Modified Date: 02/07/2020																							   
+ Modified Date: 02/20/2020																							   
 											       																	   
  Project: StructuresDB	
  																							   
@@ -12,7 +12,8 @@
  			  <Database>.<Schema>.<Table Name3>				
 			  																				   
  Description: This stored procedure is run at the end to make sure things that didn't pass out of the check table 
-			  get a record added in the audit table.								   
+			  get a record added in the audit table. They remained in the tbl_check_structures table because they could
+			  not be validated or because there was not time for a person to review the records.
 																													   												
 ***********************************************************************************************************************/
 use structuresdb
@@ -24,13 +25,15 @@ go
 set quoted_identifier on;
 go
 
-create procedure dbo.sp_i_tbl_audit_structures as
+create or alter procedure dbo.sp_i_tbl_audit_structures as
 	begin
 		begin transaction
 			insert into structuresdb.dbo.tbl_audit_structures(parks_id,
-															  audit_step_id)
+															  audit_step_id,
+															  notes)
 				select distinct parks_id,
-					   3 as audit_step_id
+					   3 as audit_step_id,
+					   notes
 				from structuresdb.dbo.tbl_check_structures
 				where validated = 0;
 		commit;
