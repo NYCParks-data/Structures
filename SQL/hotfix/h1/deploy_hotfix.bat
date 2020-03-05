235821b62d4c21965db74f5c6addb74290061b00
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-03-05 12:42:00 -0500

    Updated the sp_i_tbl_delta_structures script to account for parks_ids and doitt_ids where rows and shapes didn't change. Started adding hotfix deployment scripts.

---
+++ SQL/hotfix/h1/deploy_hotfix.bat
@@ -0,0 +1,4 @@
+tbl_parks_structures_row_hash.sql
+sp_m_tbl_parks_structures.sql
+sp_i_tbl_compare_delta_parks.sql
+sp_i_tbl_audit_structures.sql
---
+++ SQL/hotfix/h2/delete_tbl_audit_structures.sql
@@ -0,0 +1,25 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+begin transaction
+	delete 
+	from structuresdb.dbo.tbl_audit_structures
+	where cast(date_stamp as date) = '2020-02-24' and
+		  audit_step_id = 4;
+commit;
