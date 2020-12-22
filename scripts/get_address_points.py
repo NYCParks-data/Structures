eaf5dd4673fa3aeb52b70a89e5cc29f38122bdb3
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-12-21 23:58:17 -0500

    Added script for retrieving address points from Open Data. These will go into Function 1N to be normalized.

---
+++ scripts/get_address_points.py
@@ -0,0 +1,34 @@
+import json
+from urllib.parse import quote
+import requests
+import geopandas as gpd
+from shapely.geometry import shape
+import numpy as np
+
+def get_address_point(bins, geom_col):
+    failed = []
+    add_pts = []
+
+    for b in bins:
+        url = 'http://data.cityofnewyork.us/resource/n86c-vq2h?bin={}'.format(b)
+        #Encode the url, but allow the characters specified in the safe argument.
+        url = quote(url, safe = ':/?&=')
+
+        try:
+            #Send the get request to Socrata and return the results as a json
+            response = json.loads(json.dumps(requests.get(url).json()))[0]
+            response['the_geom'] = shape(response['the_geom'])
+            #Append the response to the address points list
+            add_pts.append(response)
+
+        except:
+            #Append the bins without records to the failed list
+            failed.append(b)
+            
+    add_pts_df = (gpd.GeoDataFrame(add_pts, crs = {'init':'epsg:4326'})
+                  .set_geometry(geom_col)
+                  .fillna(np.nan))
+
+    add_pts_df = add_pts_df.to_crs({'init':'epsg:2263'})
+
+    return add_pts_df, failed
